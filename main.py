860befd967b496f2fd957a5fbc14a635991c115a
00-2 <9x02@mail.ru>
2025-01-18 00:50:27 +0300

    add handle several credentials, proxies

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import vk_api
 import datetime
 import time
@@ -6,36 +7,90 @@ import settings
 import random
 from requests import Session
 ts = time.time()
+IMAGE_FOLDER = 'images'
 
-session = Session()
-session.proxies = {'http': 'socks5://4eigmwpxxk-corp.mobile.res-country-RU-state-468898-city-468902-hold-session-session-6789438029dd2:Rzw43zL9SP460cqQ@138.201.49.224:9999'}  # Замените на свой
+class Account:
+	def __init__(self, session, proxies, token):
+		self.session = session
+		self.session.proxies = proxies
+		self.token = token
+		self.vk = vk_api.VkApi(token=token, session=session)
+		self.OUR_ID = self.vk.method('users.get')
+		self.OUR_ID = self.OUR_ID[0]['id']
+		self.image_attachments = upload_images_to_vk(self, folder_path=IMAGE_FOLDER)
 
-vk_token = settings.vk_token
-vk = vk_api.VkApi(token = vk_token, session=session)
 
 EACH_NUM_POST = 20 
-OUR_ID = vk.method('users.get')
-OUR_ID = OUR_ID[0]['id']
-with open('post_text.txt', mode = 'r', encoding ='utf-8') as f_t,open('post_text 1.txt', mode = 'r', encoding ='utf-8') as f_t1,open('post_text 2.txt', mode = 'r', encoding ='utf-8') as f_t2,open('post_text 3.txt', mode = 'r', encoding ='utf-8') as f_t3, open('post_att.txt', 'r') as f_a,  open('post_att 1.txt', 'r') as f_a1,  open('post_att 2.txt', 'r') as f_a2,  open('post_att 3.txt', 'r') as f_a3,open('groups.txt', 'r') as f_g:
+
+def upload_images_to_vk(user: Account, folder_path):
+	attachments = []
+	try:
+		for filename in os.listdir(folder_path):
+			if filename.lower().endswith(('png', 'jpg', 'jpeg', 'gif')):
+				image_path = os.path.join(folder_path, filename)
+
+				# Получение URL для загрузки фото
+				upload_url = user.vk.method('photos.getWallUploadServer')['upload_url']
+
+				# Загрузка файла
+				with open(image_path, 'rb') as image_file:
+					response = user.session.post(upload_url, files={'photo': image_file}).json()
+
+				# Сохранение загруженного фото
+				saved_photo = user.vk.method('photos.saveWallPhoto', {
+					'photo': response['photo'],
+					'server': response['server'],
+					'hash': response['hash']
+				})[0]
+
+				# Добавление attachment строки
+				attachments.append(f"photo{saved_photo['owner_id']}_{saved_photo['id']}")
+	except Exception as e:
+		print(f"Error uploading images: {e}")
+	return attachments
+
+credentials = [
+	('vk1.a.PURWXzT62S9KhdA465_cv9DvuoQUO37v668E7DYnm2dCeVli9rMKxVxoYaW5tyD4HTmR6WdD3yM-oC3A_QkSvwHJvHiRwIPQExpJHUZa41-Aq2a4EmuwYy74A1xVcOBPiCfcplaem2E9SlSJdUL9xMQOcrZ8lZxsrCqVoxsqhCr9jsr1_ovM5sH4-jX2CBMATGCmVqaM41Z6Zxfq7MUoZw','socks5://4eigmwpxxk-corp.mobile.res-country-RU-state-491684-city-491687-hold-session-session-6789736828c5c:Rzw43zL9SP460cqQ@138.201.49.224:9999'),
+	('vk1.a.GnoFyUNqDETH3EPJLMoExN9iEfgRoTRGAESgYO7bqKolUfET26qmOWQSfaNRUmjlgMoCWaASg7GLSmHyo1MIBSDeH328ifIuhtSpbNMes-QbC5TCfNDNeQOG4-tv1jG6-pKnbz_70IHOFdJeYPImVvCnt-HAKcS1lxw6rs4QI86O1T4zY0GOS1exrVXRt8t9-cOvX_bartdlY8k01C_3bQ','socks5://4eigmwpxxk-corp.mobile.res-country-RU-state-524894-city-524901-hold-session-session-678ac46929907:Rzw43zL9SP460cqQ@88.99.166.254:9999'),
+]
+
+
+users = [ Account(Session(), proxies=credential[1], token=credential[0])
+	  for i, credential in enumerate(credentials)
+	]
+# Загружаем изображения из папки images
+with open('post_text.txt', mode = 'r', encoding ='utf-8') as f_t,open('post_text 1.txt', mode = 'r', encoding ='utf-8') as f_t1,open('post_text 2.txt', mode = 'r', encoding ='utf-8') as f_t2,open('post_text 3.txt', mode = 'r', encoding ='utf-8') as f_t3,open('groups.txt', 'r') as f_g:
 	post_text = [f_t.read(), f_t1.read(), f_t2.read(), f_t3.read()]
-	post_att = [f_a.read().replace('\n', ','), f_a1.read().replace('\n', ','), f_a2.read().replace('\n', ','), f_a3.read().replace('\n', ',')]
 	groups = f_g.read().strip().split('\n')
+last_group = groups[0]
+user: Account = users[random.randint(0,len(users)-1)]
 while True:
-	for group in groups:
+	i = 0
+	for group in groups[groups.index(last_group):]:
 		try:
-			rs = vk.method('wall.get', {'count': EACH_NUM_POST, 'owner_id':group})
+			last_group = group
+			rs = user.vk.method('wall.get', {'count': EACH_NUM_POST, 'owner_id':group})
 			flag = False
 			for item in rs['items']:
-				if item['from_id'] == OUR_ID:
-					flag = True
-					continue
+				for our_user in users:
+					if item['from_id'] == our_user.OUR_ID:
+						flag = True
+						continue
 			if not flag:
 				dt = datetime.datetime.now()
 
 				st = dt.strftime(dt.strftime("Day: %d/%m/%Y  time: %H:%M:%S"))
-				print(f'In group {group} created post at {st}', vk.method('wall.post', {'message': post_text[random.randint(0, len(post_text)-1)], 'owner_id':group, 'attachment': post_att[random.randint(0, len([post_att])-1)]}))
-				time.sleep (10)
+				print(f'In group {group} created post at {st}', user.vk.method('wall.post', {'message': post_text[random.randint(0, len(post_text)-1)], 'owner_id':group, 'attachment': user.image_attachments[random.randint(0, len([user.image_attachments])-1)]}))
+				time.sleep (random.randint(7,23))
+				if i % 6 == 0:
+					time.sleep(random.randint(30,40))
+				if i>12:
+					print('im finished')
+					new_user = 	users[(users.index(user)+1)%len(users)]
+					user = new_user
+					i = 0
+					break
 		except Exception as e:
-			erorr = str(traceback.format_exc())
-			print(erorr)
-	time.sleep (500)
+			err = str(traceback.format_exc())
+			print(err)
+	last_group = groups[0]
--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 #vk_token = '1fba8ac2d882dd1f0650947c276da6cdd41b0d7e11b1a5d0b355464c8a3d4e115294dd82273688ffd8c14' Витя
 #vk_token = 'vk1.a.HBi4Xfae6sfY7GoxSAJIwlL_5dPip7VpuZFAth0adKE7JQKyOr9DMG_DKhRga2aolUfuqGr_S4Q2bicDMEksdlIEvZWcgFVkE9S3NDCrZa3-Oj7R0X01mWevor1ACqmPoL0H56-Y2OIhMo1HYVx16eeldy-INbIpbTNlHwarfmznufgcG6WURM4sruygnAo6'
-vk_token = 'vk1.a.VKJhZ08ZslEBDHLT9ZPCczJdMpqeop-Quthn5lshOeN8kFK9oAHgSjUSxb2R8wJdT9R4KPC16EwM_9n8-pgqk3BS6g9vQ34Towe_7kIL3QpCDdwb2qHARbkyg88by5o8yiDhOwbMnxD-DZE0Tpc9SZ46CjOx0wj8saRBe0k0YsIabrAUgb-ad5RFv9FUMACW7LNRQCTpGxpqkQe_KK6PzQ'
 EACH_NUM_POST = 10 
 sleep = 300 
 
